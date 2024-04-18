9363edba47ea3a125bfde5ec82fbf20266ee35e5
Naviary2 <infinitechess.org@gmail.com>
2024-04-18 00:02:16 -0600

    Re-enabled default win conditions.

--- FormatConverter.js
+++ FormatConverter.js
@@ -539,9 +539,11 @@ const formatconverter = (function() {
                     
                     longformat["moves"].push(longmove);
                 }
+                if (!longformat.gameRules.winConditions) longformat.gameRules.winConditions = { white: ['checkmate'], black: ['checkmate'] } // Default win conditions if none specified
                 return longformat;
             }
         }
+        if (!longformat.gameRules.winConditions) longformat.gameRules.winConditions = { white: ['checkmate'], black: ['checkmate'] } // Default win conditions if none specified
         return longformat;
     }
 
